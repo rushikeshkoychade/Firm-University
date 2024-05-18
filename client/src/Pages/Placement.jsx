3b0032904b6b88e77709dbe8887e706177fc6ec2
Rushikesh Koychade <rushikeshkoychade@gmail.com>
2024-05-18 17:11:47 +0530

    changes in Placements page

--- client/src/Pages/Placement.jsx
+++ client/src/Pages/Placement.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Helmet } from "react-helmet";
 
 const Placement = () => {
   return (
-    <div>Placement</div>
-  )
-}
+    <>
+      <main>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>FIRM - Placements</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
 
-export default Placement
+        <section className="mx-40">
+          <div className="p-5 mt-3">
+            <div className="flex mb-14 justify-center text-4xl font-serif font-bold">
+              <h1 className="text-rose-700 uppercase">Placements In </h1> &nbsp;
+              <h1 className="text-violet-950">FIRM UNIVERSITY PUNE</h1>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Placement;
